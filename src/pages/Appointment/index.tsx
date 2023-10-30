44031d955238fecae0f2f233f7bc029975cb971c
Ngo The Loc <locngo@cqtdt.asia>
2023-10-30 17:39:42 +0700

    appointment

--- src/pages/Appointment/index.tsx
+++ src/pages/Appointment/index.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 import { Autocomplete, InputAdornment, TextField } from "@mui/material";
 import WorkIcon from "@mui/icons-material/Work";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import VaccinesIcon from "@mui/icons-material/Vaccines";
 import { Box } from "@mui/system";
 import moment from "moment";
 
 import { LIST_TIME } from "../../Contants";
 import { FORMAT_DATE, TOTAL_STEP } from "../../Contants/general.constant";
-import { DOCTOR } from "../../assets";
+import { DOCTOR, ICON_GRADUATION, ICON_PEOPLE_TEAM } from "../../assets";
 
 const Appointment = () => {
   const [step, setStep] = useState<number>(1);
@@ -16,21 +14,21 @@ const Appointment = () => {
   const [isBooking, setIsBooking] = useState<boolean>(false);
   const [isPassStep1, setIsPassStep1] = useState<boolean>(false);
 
-  const [listDepartment, setListDepartment] = useState([]);
+  const [listTypeOfAppointment, setListTypeOfAppointment] = useState([]);
+  const [listSpecialty, setListSpecialty] = useState([]);
   const [listDoctor, setListDoctor] = useState([]);
-  const [listService, setListService] = useState([]);
-
-  const [department, setDepartment] = useState<string>("");
-  const [doctor, setDoctor] = useState<string>("");
-  const [service, setService] = useState<string>("");
 
   const [date, setDate] = useState<string>("");
   const [time, setTime] = useState<string>("");
-
+  
   const [appointmentTime, setAppointmentTime] = useState<string>(
     moment(`${date} ${time}`).format(FORMAT_DATE)
-  );
-
+    );
+  const [specialty, setSpecialty] = useState<string>("");
+  const [typeOfAppointment, setTypeOfAppointment] = useState<string>("");
+  const [doctor, setDoctor] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+    
   useEffect(() => {
     setAppointmentTime(moment(`${date} ${time}`).format(FORMAT_DATE));
   }, [time, date]);
@@ -49,28 +47,20 @@ const Appointment = () => {
     setStep(step - 1);
   };
 
-  const handleChangeDepartment = (value: string) => {
-    value ? setDepartment(value) : setDepartment("");
+  const handleChangeTypeOfAppointment = (value: string) => {
+    value ? setTypeOfAppointment(value) : setTypeOfAppointment("");
   };
 
-  const handleKeyUpDepartment = (value: string) => {
-    value ? setDepartment(value) : setDepartment("");
+  const handleKeyUpTypeOfAppointment = (value: string) => {
+    value ? setTypeOfAppointment(value) : setTypeOfAppointment("");
   };
 
-  const handleChangeDoctor = (value: string) => {
-    value ? setDoctor(value) : setDoctor("");
+  const handleChangeSpecialty = (value: string) => {
+    value ? setSpecialty(value) : setSpecialty("");
   };
 
-  const handleKeyUpDoctor = (value: string) => {
-    value ? setDoctor(value) : setDoctor("");
-  };
-
-  const handleChangeService = (value: string) => {
-    value ? setService(value) : setService("");
-  };
-
-  const handleKeyUpService = (value: string) => {
-    value ? setService(value) : setService("");
+  const handleKeyUpSpecialty = (value: string) => {
+    value ? setSpecialty(value) : setSpecialty("");
   };
 
   const _renderAppointmentHeader = () => {
@@ -121,23 +111,34 @@ const Appointment = () => {
   const _renderStep1 = () => {
     return (
       <div className="container">
-        <div className="row gx-5">
-          <div className="col-6">
+        <div>
+          <h3>Patient Details</h3>
+          <label htmlFor="" className="form-label">Select Patient </label>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" id="" />
+          </div>
+          <button className="button-apply">Apply</button>
+        </div>
+
+        <div>
+          <h3>Booking details</h3>
+          <div>
+            <label htmlFor="">Type of appointment</label>
             <Autocomplete
-              options={listDepartment}
+              options={listTypeOfAppointment}
               autoComplete
               onChange={(event: any) =>
-                handleChangeDepartment(event.target.innerText)
+                handleChangeTypeOfAppointment(event.target.innerText)
               }
               onKeyUp={(event: any) =>
-                handleKeyUpDepartment(event.target.value)
+                handleKeyUpTypeOfAppointment(event.target.value)
               }
               disablePortal
               renderInput={(params) => (
                 <div>
                   <TextField
                     {...params}
-                    placeholder="Select Department"
+                    placeholder="Type of appointment"
                     InputLabelProps={{ shrink: false }}
                     variant="outlined"
                     InputProps={{
@@ -153,14 +154,17 @@ const Appointment = () => {
                 </div>
               )}
             />
+          </div>
 
+          <div>
+            <label htmlFor="">Select specialty</label>
             <Autocomplete
-              options={listDoctor}
+              options={listSpecialty}
               autoComplete
               onChange={(event: any) =>
-                handleChangeDoctor(event.target.innerText)
+                handleChangeSpecialty(event.target.innerText)
               }
-              onKeyUp={(event: any) => handleKeyUpDoctor(event.target.value)}
+              onKeyUp={(event: any) => handleKeyUpSpecialty(event.target.value)}
               disablePortal
               renderOption={(props, option) => (
                 <Box
@@ -180,42 +184,14 @@ const Appointment = () => {
                 <div>
                   <TextField
                     {...params}
-                    placeholder="Select Doctor"
+                    placeholder="Select specialty"
                     InputLabelProps={{ shrink: false }}
                     variant="outlined"
                     InputProps={{
                       ...params.InputProps,
                       startAdornment: (
                         <InputAdornment position="start">
-                          <VaccinesIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                    fullWidth
-                  />
-                </div>
-              )}
-            />
-            <Autocomplete
-              options={listService}
-              autoComplete
-              onChange={(event: any) =>
-                handleChangeService(event.target.innerText)
-              }
-              onKeyUp={(event: any) => handleKeyUpService(event.target.value)}
-              disablePortal
-              renderInput={(params) => (
-                <div>
-                  <TextField
-                    {...params}
-                    placeholder="Select Service"
-                    InputLabelProps={{ shrink: false }}
-                    variant="outlined"
-                    InputProps={{
-                      ...params.InputProps,
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <FavoriteBorderIcon />
+                          <WorkIcon />
                         </InputAdornment>
                       ),
                     }}
@@ -225,25 +201,61 @@ const Appointment = () => {
               )}
             />
           </div>
-          <div className="col-6">
-            <p className="fw-bold">Appointment time</p>
-            <div>
-              <p>Date</p>
-              <div className="row"></div>
-            </div>
+        </div>
 
-            <div>
-              <p>Time</p>
-              <div className="row">
-                {LIST_TIME.map((time: any) => {
-                  return (
-                    <div className="col-3" onClick={() => setTime(time.value)}>
-                      <p className="text-center py-3 bg-light rounded text-dark fw-700">
-                        {time.title}
-                      </p>
+        <div>
+          <h3>Select Doctor</h3>
+          <div className="row">
+            {listDoctor.map((doctor:any, idx:number)=>{
+              const name = doctor.practitionerTarget.nameFirstRep.nameAsSingleString;
+              const specialty = doctor.specialty[0].coding[0].display;
+              return (
+                  <div className='col-6 row gy-3 py-3 mb-3'>
+                    <div className='col-4'>
+                        <img src={DOCTOR} alt="" />
                     </div>
-                  );
-                })}
+                    <div className='col-8'>
+                        <h3 className='mb-3'>{name}</h3>
+                        <p className='ms-3'><span><ICON_GRADUATION /></span>  Level II specialist, Meritorious Doctor</p>
+                        <p className='ms-3'><span><ICON_PEOPLE_TEAM /></span> {specialty}</p>
+                    </div>
+                  </div>
+              )
+            })}
+          </div>
+        </div>
+
+        <div>
+          <h3>Select date and time</h3>
+          <div className="row">
+            <div className="col-4">
+              <label htmlFor="">Choose a date </label>
+              <div className="input-group">
+                <input
+                  type="date"
+                  className="form-control"
+                  autoComplete="new-password"
+                />
+              </div>
+              <span>See next 7 days</span>
+            </div>
+            <div className="col-8">
+              <label htmlFor="">Select a timeslot</label>
+              <span>Duration 60 mins</span>
+              <div className="row">
+              {LIST_TIME.map((item: any, idx: number) => {
+                return (
+                    <div className="col-4">
+                      <p>{item.title}</p>
+                    </div>
+                );
+              })}
+              </div>
+            </div>
+            <div className="col-12">
+              <label htmlFor="description">Description</label>
+              <div className="form-floating">
+                <textarea className="form-control" placeholder="Leave a comment here" id="description"></textarea>
               </div>
             </div>
           </div>
@@ -323,7 +335,7 @@ const Appointment = () => {
           <h3 className="text-center mb-3">Booking Success</h3>
           <p className="text-center mb-5 text-gray">
             <span className="d-block mb-1">
-              Your appointment has been successfully{" "}
+              Your appointment has been successfully
             </span>
             <span className="d-block mb-1">
               booked, please regularly check your
